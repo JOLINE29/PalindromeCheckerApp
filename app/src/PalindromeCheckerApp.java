f22230b7e8ba32d0ea7b8784480546d612bffd51
JOLINE29 <jolinefloria@gmail.com>
2026-03-09 16:38:54 +0530

    uc10- string

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -2,34 +2,37 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    // Recursive function
-    static boolean isPalindrome(String str, int start, int end) {
+    public static boolean isPalindrome(String str) {
 
-        // Base condition
-        if (start >= end) {
-            return true;
+        // Normalize string
+        str = str.toLowerCase();
+        str = str.replaceAll("[^a-z0-9]", "");
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
-        // If characters don't match
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String word = sc.nextLine();
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
 
-        if (isPalindrome(word, 0, word.length() - 1)) {
-            System.out.println(word + " is a Palindrome");
+        if (isPalindrome(input)) {
+            System.out.println("It is a Palindrome");
         } else {
-            System.out.println(word + " is Not a Palindrome");
+            System.out.println("It is Not a Palindrome");
         }
 
         sc.close();
